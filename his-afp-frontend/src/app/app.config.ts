ea260640a55ecfcad76bb095b77b75706917ee63
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-01-26 12:05:37 +0100

    chore: Configurazione librerie PrimeNG, Prettier e Tailwind

--- his-afp-frontend/src/app/app.config.ts
+++ his-afp-frontend/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura, //tema di default di PrimeNG
+        options : {
+          ripple: true, //animazione grafica
+          darkModeSelector: '.my-app-dark', //selettore per la modalità scura
+        }
+      }
+    }),
   ]
 };
