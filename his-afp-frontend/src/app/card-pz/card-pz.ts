9ae95d5d3ad86089c72012f506127ae9583ae5a6
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-01-28 11:50:07 +0100

    feat: added signal and button in the card
    Added signal and button to understand how it works and how tu use signals

--- his-afp-frontend/src/app/card-pz/card-pz.ts
+++ his-afp-frontend/src/app/card-pz/card-pz.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'his-card-pz',
-  imports: [CardModule],
+  imports: [CardModule, ButtonModule],
   templateUrl: './card-pz.html',
   styleUrl: './card-pz.scss',
 })
 export class CardPz {
-  
+  nome: string = 'pietro';
+  paziente = signal('');
+
+  cambiaNome(){
+    this.nome = "Gian";
+
+    this.paziente.set("Lucio");
+  }
 }
