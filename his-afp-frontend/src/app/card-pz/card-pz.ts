c949eb5948ec26a59c81bdb8d7907c7005425afd
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-01-28 11:59:01 +0100

    feat: added interface for patient and decleared inherent signal, and added to the card

--- his-afp-frontend/src/app/card-pz/card-pz.ts
+++ his-afp-frontend/src/app/card-pz/card-pz.ts
@@ -1,20 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { JsonPipe } from '@angular/common';
+
+interface Paziente {
+  id: string;
+  nome: string;
+  cognome: string;
+  braccialetto: string;
+  eta: number;
+  codiceColore: string;
+  note: string;
+  patologia: string;
+}
 
 @Component({
   selector: 'his-card-pz',
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, JsonPipe],
   templateUrl: './card-pz.html',
   styleUrl: './card-pz.scss',
 })
 export class CardPz {
   nome: string = 'pietro';
-  paziente = signal('');
+  paziente = signal<Paziente>({
+    id: '23',
+    nome: 'Stefano',
+    cognome: 'Plotegher',
+    braccialetto: '123',
+    eta: 20,
+    codiceColore: 'ROSSO',
+    note: 'Trauma',
+    patologia: 'C19'
+  });
 
   cambiaNome(){
     this.nome = "Gian";
 
-    this.paziente.set("Lucio");
+    
   }
 }
