d6736c08bc99b542d1a5dab83fa256c03812bbaa
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-02-26 11:05:43 +0100

    feat: added mapping of API response

--- his-afp-frontend/src/app/core/PatientManager/patient-manager.ts
+++ his-afp-frontend/src/app/core/PatientManager/patient-manager.ts
@@ -16,12 +16,39 @@ export class PatientManager {
   }
 
   public fetchPazienti(){
-    this.#http.get<APIResponse<PazienteDTO>>('http://localhost:3000/admissions').subscribe({
+    this.#http.get<APIResponse<PazienteDTO[]>>('http://localhost:3000/admissions').subscribe({
       next: (res) => {
-        this.#listaPZ.set(res.data);
+        const pz = res.data.map((p) => this.mapPazienteDTOtoPaziente(p));
+        this.#listaPZ.set(pz);
       },
-      error: (err) => {}
+      error: (err) => {
+        console.error('Errore nel recupero dei pazienti:', err);
+      }
     }
     )
+  }
+
+    public mapPazienteDTOtoPaziente(pz: PazienteDTO):Paziente{
+    return {
+      id: pz.id.toString(),
+      nome: pz.nome,
+      cognome: pz.cognome,
+      braccialetto: pz.braccialetto,
+      codiceColore: pz.coloreCode,
+      note: pz.noteTriage,
+      patologia: pz.patologiaCode,
+      eta: this.calcolaEta(pz.dataNascita)
+    }
+  }
+
+  public calcolaEta (dataNascita: string): number {
+    const oggi = new Date();
+    const nascita = new Date(dataNascita);
+    let eta = oggi.getFullYear() - nascita.getFullYear();
+    const mese = oggi.getMonth() - nascita.getMonth();
+    if (mese < 0 || (mese === 0 && oggi.getDate() < nascita.getDate())) {
+      eta--;
+    }
+    return eta;
   }
 }
