5dd062b2d970a26e7d4dea0b90eef4a969f17d61
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-03-04 15:18:31 +0100

    chore: :truck: added TabellaPz component with HTML, SCSS, and unit tests

---
+++ his-afp-frontend/src/app/pattern/tabella-pz/tabella-pz.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'his-tabella-pz',
+  imports: [],
+  templateUrl: './tabella-pz.html',
+  styleUrl: './tabella-pz.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TabellaPz {
+
+}
