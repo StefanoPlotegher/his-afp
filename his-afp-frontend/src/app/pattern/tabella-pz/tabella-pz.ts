753e33083a8083a70bbcad52156f7bec8364203d
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-03-04 15:47:10 +0100

    feat: :sparkles: updated layout and integrated TabellaPz component in lista-pz

--- his-afp-frontend/src/app/pattern/tabella-pz/tabella-pz.ts
+++ his-afp-frontend/src/app/pattern/tabella-pz/tabella-pz.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, effect, inject, model } from '@angular/core';
 import { PatientManager } from '../../core/PatientManager/patient-manager';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 import { CardPz } from "../../ui/card-pz/card-pz";
 
 @Component({
   selector: 'his-tabella-pz',
-  imports: [FormsModule, CardPz],
+  imports: [FormsModule, InputTextModule, CardPz ],
   templateUrl: './tabella-pz.html',
   styleUrl: './tabella-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
