af7b8fc7e79b3d2006fd5fd9dfeeeb37ae0425d3
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-03-04 15:32:03 +0100

    refactor: milgiorato il componente che mostra i pazienti

--- his-afp-frontend/src/app/pattern/tabella-pz/tabella-pz.ts
+++ his-afp-frontend/src/app/pattern/tabella-pz/tabella-pz.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, model } from '@angular/core';
+import { PatientManager } from '../../core/PatientManager/patient-manager';
+import { FormsModule } from '@angular/forms';
+import { CardPz } from "../../ui/card-pz/card-pz";
 
 @Component({
   selector: 'his-tabella-pz',
-  imports: [],
+  imports: [FormsModule, CardPz],
   templateUrl: './tabella-pz.html',
   styleUrl: './tabella-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabellaPz {
+  readonly PatientManager = inject(PatientManager)
+  nomePaziente = model<string>('');
 
+  constructor(){
+    effect(() => {
+      this.PatientManager.filterByName(this.nomePaziente());
+    });
+  }
 }
