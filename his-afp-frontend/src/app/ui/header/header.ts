5dd5b292d38267964029e82777f9794cf1c7aed5
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-03-05 11:39:47 +0100

    feat: ✨ update header with new department name and location, and add button labels

--- his-afp-frontend/src/app/ui/header/header.ts
+++ his-afp-frontend/src/app/ui/header/header.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonModule } from "primeng/button";
 import { DarkmodeSelector } from "../darkmode-selector/darkmode-selector";
 import { RouterModule } from '@angular/router';
+import { DividerModule } from "primeng/divider";
 
 @Component({
   selector: 'his-header',
-  imports: [ButtonModule, DarkmodeSelector, RouterModule],
+  imports: [ButtonModule, DarkmodeSelector, RouterModule, DividerModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
