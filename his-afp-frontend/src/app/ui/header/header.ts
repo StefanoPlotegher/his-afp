88ec950f0bd6f5213449c3314b5afa5c6637145f
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-04-13 12:01:57 +0200

    feat: ✨ update environment configuration and enhance frontend header display

--- his-afp-frontend/src/app/ui/header/header.ts
+++ his-afp-frontend/src/app/ui/header/header.ts
@@ -4,10 +4,11 @@ import { DarkmodeSelector } from "../darkmode-selector/darkmode-selector";
 import { RouterLink } from '@angular/router';
 import { DividerModule } from "primeng/divider";
 import { environment } from '../../../environments/environment.development';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'his-header',
-  imports: [ButtonModule, DarkmodeSelector, RouterLink, DividerModule],
+  imports: [ButtonModule, DarkmodeSelector, RouterLink, DividerModule, TagModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +18,7 @@ export class Header {
   //ci passiamo le variabli dell'evironment così è tutto più bello
   reparto = environment.reparto;
   struttura = environment.struttura;
+
+  type = (window as any).env?.type;
+  version = (window as any).env?.version;
 }
