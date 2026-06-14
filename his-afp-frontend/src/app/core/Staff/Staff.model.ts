3e6795b6c6eb786597966a0e11460e2d9917fd21
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-06-14 16:34:46 +0200

    feat: :sparkles: Creata logica di fetch del personale
    Creata interfaccia Staff per mappare la risposta del backend al fetch del personale e creata la funzione per esguire il fetch

---
+++ his-afp-frontend/src/app/core/Staff/Staff.model.ts
@@ -0,0 +1,6 @@
+export interface Staff {
+    id: number,
+    username: string,
+    role: string,
+    isActive: boolean
+}
--- his-afp-frontend/src/app/core/Staff/staff-manager.ts
+++ his-afp-frontend/src/app/core/Staff/staff-manager.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Staff } from './Staff.model';
+import { APIResponse } from '../models/Response.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StaffManager {
-  
+  #http = inject(HttpClient);
+  #staff = signal<Staff[]>([]);
+  staff = this.#staff.asReadonly();
+
+
+  public fetchStaff(){
+    this.#http.get<APIResponse<Staff[]>>("/api/users").subscribe({
+      next: (res) => {
+        this.#staff.set(res.data);
+      },
+      error: (err) => {
+        console.error('Errore nel recupero dello staff:', err);
+      }
+    })
+  }
 }
