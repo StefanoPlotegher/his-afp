c9b244c7cf36e637d3ecc4d92cb7649e1f358846
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-06-14 18:41:10 +0200

    feat: :sparkles: MOdifica componente Staff per chiamare il componente tabella-staff
    Il componente ora chiama e mostra il componente tabella-staff come fattoo con i pazienti

--- his-afp-frontend/src/app/features/staff/staff.ts
+++ his-afp-frontend/src/app/features/staff/staff.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TabellaStaff } from '../../pattern/tabella-staff/tabella-staff';
 
 @Component({
   selector: 'his-staff',
-  imports: [],
+  imports: [TabellaStaff],
   templateUrl: './staff.html',
   styleUrl: './staff.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
