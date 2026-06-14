a72080719d7cf649acaea5a04341069d1b8c0dae
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-06-14 15:57:16 +0200

    feat: :sparkles: Aggiunto nuovo componente Staff
    Creato il compnente per la visualizzazione dello staff

---
+++ his-afp-frontend/src/app/features/staff/staff.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'his-staff',
+  imports: [],
+  templateUrl: './staff.html',
+  styleUrl: './staff.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Staff {
+
+}
