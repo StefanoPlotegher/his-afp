c9cc06343681582702496b513a2b2fca404bc7a3
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-06-21 15:10:41 +0200

    chore: Added component to modify a staff member

---
+++ his-afp-frontend/src/app/features/modifica-staff/modifica-staff.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'his-modifica-staff',
+  imports: [],
+  templateUrl: './modifica-staff.html',
+  styleUrl: './modifica-staff.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModificaStaff {
+
+}
