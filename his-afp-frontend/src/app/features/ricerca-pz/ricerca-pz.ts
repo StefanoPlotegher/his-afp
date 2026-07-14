cae0f4d324737c17c7db001e7d193ec280005271
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-07-14 18:12:47 +0200

    chore: Creato componente ricerca-pz

---
+++ his-afp-frontend/src/app/features/ricerca-pz/ricerca-pz.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'his-ricerca-pz',
+  imports: [],
+  templateUrl: './ricerca-pz.html',
+  styleUrl: './ricerca-pz.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RicercaPz {
+
+}
