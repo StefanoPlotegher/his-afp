4f4b026a225db31c4a2db42d4d89c7ced109f9ca
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2026-07-18 12:03:34 +0200

    feat: :sparkles: Creata tabella di visualizzazione dei pazienti dimessi
    Creata tabella con p-table ordinabile per orario di dimissione, toggle-switch per fermare o avviare il refresh dei dimessi, pulsante refresh classico per eseguire un refresh singolo

--- his-afp-frontend/src/app/pattern/tabella-dimessi/tabella-dimessi.ts
+++ his-afp-frontend/src/app/pattern/tabella-dimessi/tabella-dimessi.ts
@@ -1,12 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, model } from '@angular/core';
+import { PatientManager } from '../../core/Pazienti/patient-manager';
+import { TableModule } from 'primeng/table';
+import { DatePipe } from '@angular/common';
+import { ButtonModule } from "primeng/button";
+import { ToggleSwitchModule } from "primeng/toggleswitch";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'his-tabella-dimessi',
-  imports: [],
+  imports: [TableModule, DatePipe, ButtonModule, ToggleSwitchModule, FormsModule],
   templateUrl: './tabella-dimessi.html',
   styleUrl: './tabella-dimessi.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabellaDimessi {
+  readonly PatientManager = inject(PatientManager)
+  enableRefresh = model<boolean>(true);
 
+  constructor(){
+    effect(() => {
+
+      if(this.enableRefresh()){
+        this.PatientManager.refreshDimessi();
+      }else{
+        this.PatientManager.stopRefreshDim();
+      }
+    });
+  }
 }
